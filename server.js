095146968010e9fdd3aa9caff3a3d8050968e0c2
mohamedhawas123 <mohamedhawas123@gmail.com>
2023-08-28 17:26:57 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import connectDB from './config/db.js'
+import userRoutes from './routes/userRoutes.js'
+import cors from 'cors'
+
+connectDB()
+
+const app = express()
+
+app.use(express.json())
+
+app.use(cors())
+
+
+
+
+app.listen(5000, console.log('server is running'))
