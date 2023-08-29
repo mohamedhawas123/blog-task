7121d3a2c355720dfaf3bc376a64595f7e4b84e6
mohamedhawas123 <mohamedhawas123@gmail.com>
2023-08-29 15:52:14 +0300

    edit readme

--- server.js
+++ server.js
@@ -21,7 +21,6 @@ app.use(express.json())
 
 app.use(cors())
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-
 app.use('/api/users', userRoutes)
 app.use('/api/blog', blogRoutes)
 
